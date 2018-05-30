e1b4b57cc66c2d596b4e79d21211bbe8a4bf8143
wangxiao <wx497657341@qq.com>
2018-05-30 12:05:47 +0800

    admin menus

---
+++ database/migrations/2018_05_30_115459_create_admin_menus_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdminMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admin_menus', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->default('');
+            $table->string('href')->default('');
+            $table->integer('parent_id')->unsigned()->default(0)->comment('parent menu id');
+            $table->smallInteger('sort')->unsigned()->default(0)->comment('sort');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admin_menus');
+    }
+}
